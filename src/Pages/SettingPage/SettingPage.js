66785f59473afd64134d38e65db29735821be7c7
boyhaimai <huubangxzy@gmail.com>
2025-06-16 15:35:13 +0700

    update

--- src/Pages/SettingPage/SettingPage.js
+++ src/Pages/SettingPage/SettingPage.js
@@ -296,7 +296,7 @@ function SettingPage() {
   };
 
   const handleCopy = () => {
-    const code = `<script src="https://cdn.jsdelivr.net/gh/boyhaimai/model_admin_just_chat_v10@main/dist/model_admin_just_chat.js" data-server-url="${form.serverUrl}" data-id-config="${id_config}" defer></script>`;
+    const code = `<script src="https://cdn.jsdelivr.net/gh/boyhaimai/model_admin_just_chat_v11@main/dist/model_admin_just_chat.js" data-server-url="${form.serverUrl}" data-id-config="${id_config}" defer></script>`;
     navigator.clipboard
       .writeText(code)
       .then(() => {
@@ -847,7 +847,7 @@ function SettingPage() {
                     fullWidth
                     multiline
                     rows={8}
-                    value={`<script src="https://cdn.jsdelivr.net/gh/boyhaimai/model_admin_just_chat_v10@main/dist/model_admin_just_chat.js" data-server-url="${form.serverUrl}" data-id-config="${id_config}" defer></script>`}
+                    value={`<script src="https://cdn.jsdelivr.net/gh/boyhaimai/model_admin_just_chat_v11@main/dist/model_admin_just_chat.js" data-server-url="${form.serverUrl}" data-id-config="${id_config}" defer></script>`}
                     InputProps={{
                       style: { fontFamily: "monospace", fontSize: 14 },
                       readOnly: true,
